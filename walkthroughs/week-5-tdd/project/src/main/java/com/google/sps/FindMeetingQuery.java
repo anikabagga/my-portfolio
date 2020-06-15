a8dd874410886b02566423ec93c51ae7c7d3e291
anikabagga <anikabagga@gmail.com>
2020-06-15 18:55:34 +0000

    meeting times without optional attendees

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,87 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Collection;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    //throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> mandatory_attendees = new ArrayList<>();
+    Collection<TimeRange> optional_attendees = new ArrayList<>();
+
+    return findTime(events, request);
+  }
+
+  public Collection<TimeRange> findTime(Collection<Event> events, MeetingRequest request) {
+    
+    List<TimeRange> possibleMeetingTimes = new ArrayList<>();
+    int requestDuration = (int)request.getDuration();
+    Collection<String> requestAttendees = request.getAttendees();
+    int numEvents = events.size();
+    int availableStart = TimeRange.START_OF_DAY;
+    int event_start;
+    int event_end;
+    boolean skip;
+    int counter = 0;
+    Set<String> eventAttendees;
+
+    // No meeting times if duration is greater than whole day 
+    if (requestDuration > TimeRange.WHOLE_DAY.duration()) {
+        return possibleMeetingTimes;
+    }
+
+    // Check if no events happening
+    if (numEvents == 0) {
+        possibleMeetingTimes.add(TimeRange.WHOLE_DAY);
+        return possibleMeetingTimes;
+    }
+    
+    for (Event e : events) {
+      counter++;
+      skip = true;
+      eventAttendees = e.getAttendees();
+      for (String attendee: eventAttendees) {
+        if (requestAttendees.contains(attendee)) {
+          skip = false;
+          break;
+        } else if (counter == numEvents) {
+          possibleMeetingTimes.add(TimeRange.fromStartEnd(availableStart, TimeRange.END_OF_DAY, true));
+        }
+      }
+
+      if (!skip) {
+        event_start = e.getWhen().start();
+        event_end = e.getWhen().end();
+
+        if (event_start == TimeRange.START_OF_DAY) {
+          availableStart = event_end;
+        }
+
+        if (event_start < availableStart) {
+          if (event_end > availableStart) {
+            availableStart = event_end;
+          }
+
+        }
+
+        if (availableStart <= event_start) {
+          if (event_start - availableStart >= requestDuration) {
+            possibleMeetingTimes.add(TimeRange.fromStartEnd(availableStart, event_start, false));
+          }
+          availableStart = event_end;
+        }
+
+        if (counter == numEvents) {
+          if (availableStart + requestDuration <= TimeRange.END_OF_DAY) {
+            possibleMeetingTimes.add(TimeRange.fromStartEnd(availableStart, TimeRange.END_OF_DAY, true));
+          }
+        }
+      }
+    }
+
+    return possibleMeetingTimes;
   }
 }
